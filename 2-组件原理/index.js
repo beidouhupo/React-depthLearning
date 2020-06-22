5f5feb64d3acc4e2c578fe3af837ac07fd8eecb4
beidouhopo <2473502670@qq.com>
2020-06-22 14:21:57 +0800

    函数式组件的渲染机制

---
+++ 2-组件原理/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dialog from './component/Dialog';
+
+ReactDOM.render(<div>
+{/* 注释: JSX中调取组件, 只需要把组件当作一个标签调取即可(单闭合和双闭合都可以) */}
+  <Dialog con='哈哈哈' style={{color: 'red'}}/>
+
+  <Dialog con='嘿嘿嘿' lx={2}>
+    {/* 属性值不是字符串, 我们需要使用大括号包起来 */}
+    <span>1</span>
+    <span>2</span>
+  </Dialog>
+</div>, document.getElementById('root'));
+
